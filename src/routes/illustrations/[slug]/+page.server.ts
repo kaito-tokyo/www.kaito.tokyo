e048854fc060eeefabfcb1866528448006dfae07
Kaito Udagawa <umireon@gmail.com>
2024-01-04 21:02:53 +0000

    Revert "SSG"
    This reverts commit 0d62d2dac63634e9006ea333bd5d8614771c6927.

--- src/routes/illustrations/[slug]/+page.server.ts
+++ src/routes/illustrations/[slug]/+page.server.ts
@@ -1,17 +1,17 @@
-import { newtClient } from '$lib/server/newt';
-import type { IllustrationArticle } from '$lib/server/newt';
-import type { PageServerLoad } from './$types';
+import { newtClient } from '$lib/server/newt'
+import type { IllustrationArticle } from '$lib/server/newt'
+import type { PageServerLoad } from './$types'
 
 export const load = (async ({ params }) => {
-	const article = await newtClient.getFirstContent<IllustrationArticle>({
-		appUid: 'illustration',
-		modelUid: 'article',
-		query: {
-			slug: params.slug,
-			select: ['_id', 'title', 'slug', 'images', 'publishDate', 'description']
-		}
-	});
-	return {
-		article
-	};
-}) satisfies PageServerLoad;
+  const article = await newtClient.getFirstContent<IllustrationArticle>({
+    appUid: 'illustration',
+    modelUid: 'article',
+    query: {
+      slug: params.slug,
+      select: ['_id', 'title', 'slug', 'images', 'publishDate', 'description']
+    }
+  })
+  return {
+    article
+  }
+}) satisfies PageServerLoad
