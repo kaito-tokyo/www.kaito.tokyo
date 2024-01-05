657358e831803893a8fe7625f32863fba01e07f8
Kaito Udagawa <umireon@gmail.com>
2024-01-06 04:17:10 +0900

    Responsive (#15)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,8 +23,8 @@
 				<article>
 					<Image
 						src={article.images[0].src}
-						width={200}
-						height={200}
+						width={500}
+						height={500}
 					/>
 					<p class="title">{article.title}</p>
 					<p class="publishedAt">{formatDate(article.publishedAt)}</p>
@@ -37,18 +37,24 @@
 <style>
 	#illustration-grid {
 		display: grid;
-		column-gap: 30px;
-		grid-template-columns: 210px 210px 210px;
+		column-gap: 20px;
+		row-gap: 20px;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	@media (min-width: 720px) {
+		#illustration-grid {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
 	}
 
 	#illustration-grid article {
 		font-family: 'Zen Kaku Gothic New', serif;
-		padding: 5px;
-		background: hsl(none none 95%);
-		box-shadow: 10px 5px 5px red;
-		width: 200px;
-		height: 240px;
+		padding: 1vw;
+		background: hsl(none none 98%);
+		box-shadow: 10px 10px 10px hsl(none none 40%);
 		transition: transform 0.1s;
+		border-radius: 10px;
 	}
 
 	#illustration-grid article:hover {
@@ -58,35 +64,5 @@
 	#illustration-grid p {
 		margin: 0;
 		padding: 0;
-		font-size: 15px;
-		line-height: 17px;
-	}
-
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
