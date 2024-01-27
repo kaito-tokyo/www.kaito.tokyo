aa8d62388a272a8d93854ca45b07a026bc49db05
Kaito Udagawa <umireon@gmail.com>
2024-01-28 03:00:54 +0900

    Add WIF module

---
+++ modules/wif-github-umireon/main.tf
@@ -0,0 +1,16 @@
+resource "google_iam_workload_identity_pool" "github_umireon" {
+  workload_identity_pool_id = "github-umireon"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_umireon" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_umireon.workload_identity_pool_id
+  workload_identity_pool_provider_id = "actions-githubusercontent-com"
+  attribute_condition                = "assertion.repository_owner == \"umireon\""
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.repository_owner" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
