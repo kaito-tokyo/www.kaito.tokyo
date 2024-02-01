5b6aef8244c9938a22dc56d93131847d699425d8
Kaito Udagawa <umireon@gmail.com>
2024-02-02 02:32:23 +0900

    Add

--- modules/youtube-fetcher/main.tf
+++ modules/youtube-fetcher/main.tf
@@ -1,3 +1,15 @@
+resource "google_service_account" "youtube_fetcher_workflow" {
+  project    = var.project_id
+  account_id = "youtube-fetcher-workflow"
+}
+
+resource "google_workflows_workflow" "youtube_fetcher" {
+  project = var.project_id
+  region = "asia-east1"
+  service_account = google_service_account.youtube_fetcher_workflow
+  source_contents = file("${path.module}/youtube-fetcher.yaml")
+}
+
 resource "google_service_account" "youtube_fetcher" {
   project    = var.project_id
   account_id = "youtube-fetcher"
