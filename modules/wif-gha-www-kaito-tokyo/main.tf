ebe1546f1f2d70d51fecaf1c20e9359f9fd99dc9
Kaito Udagawa <umireon@gmail.com>
2024-02-02 03:29:10 +0900

    Add youtube fetcher run (#46)
    * Add
    
    * Update youtube-video-fetcher.yml
    
    * Update youtube-video-fetcher.yml
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Add
    
    * Update youtube-fetcher.yml
    
    * Update youtube-fetcher.yml
    
    * Update youtube-fetcher.yml
    
    * a
    
    * Update main.tf
    
    * Fix
    
    * Update main.tf
    
    * Update youtube-fetcher.yml
    
    * Update main.tf
    
    * Update youtube-fetcher.yml
    
    * Update main.tf
    
    * Update main.tf
    
    * Fix
    
    * Update main.tf
    
    * Update main.tf
    
    * Update youtube-fetcher.yml
    
    * Fix
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Fix
    
    * Add code
    
    * Update youtube-fetcher.yml
    
    * Add
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update youtube-fetcher.yaml
    
    * Update youtube-fetcher.yaml
    
    * Update youtube-fetcher.yml
    
    * Fix
    
    * Update main.tf
    
    * Update main.tf
    
    * Update youtube-fetcher.yml
    
    * Add swc
    
    * Update package.json

--- modules/wif-gha-www-kaito-tokyo/main.tf
+++ modules/wif-gha-www-kaito-tokyo/main.tf
@@ -18,9 +18,61 @@ resource "google_service_account_iam_policy" "gha_www_kaito_tokyo" {
   policy_data        = data.google_iam_policy.gha_www_kaito_tokyo.policy_data
 }
 
-
-resource "google_project_iam_member" "gha_www_kaito_tokyo_run_admin" {
+resource "google_project_iam_member" "gha_www_kaito_tokyo_service_account_user" {
   project = var.project_id
-  role    = "roles/run.admin"
+  role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.gha_www_kaito_tokyo.email}"
+}
+
+resource "google_project_iam_member" "gha_www_kaito_tokyo_config_admin" {
+  project = var.project_id
+  role    = "roles/config.admin"
+  member  = "serviceAccount:${google_service_account.gha_www_kaito_tokyo.email}"
+}
+
+resource "google_project_iam_member" "gha_www_kaito_tokyo_config_agent" {
+  project = var.project_id
+  role    = "roles/config.agent"
+  member  = "serviceAccount:${google_service_account.gha_www_kaito_tokyo.email}"
+}
+
+resource "google_project_iam_member" "gha_www_kaito_tokyo_cloudfunctions_admin" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.admin"
+  member  = "serviceAccount:${google_service_account.gha_www_kaito_tokyo.email}"
+}
+
+resource "google_service_account" "infra_manager" {
+  project    = var.project_id
+  account_id = "infra-manager"
+}
+
+resource "google_project_iam_member" "infra_manager_config_agent" {
+  project = var.project_id
+  role    = "roles/config.agent"
+  member  = "serviceAccount:${google_service_account.infra_manager.email}"
+}
+
+resource "google_project_iam_member" "infra_manager_project_iam_admin" {
+  project = var.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:${google_service_account.infra_manager.email}"
+}
+
+resource "google_project_iam_member" "infra_manager_service_account_admin" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:${google_service_account.infra_manager.email}"
+}
+
+resource "google_project_iam_member" "infra_manager_workflows_admin" {
+  project = var.project_id
+  role    = "roles/workflows.admin"
+  member  = "serviceAccount:${google_service_account.infra_manager.email}"
+}
+
+resource "google_project_iam_member" "infra_manager_service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.infra_manager.email}"
 }
---
+++ modules/youtube-fetcher/main.tf
@@ -0,0 +1,22 @@
+resource "google_service_account" "youtube_fetcher_workflow" {
+  project    = var.project_id
+  account_id = "youtube-fetcher-workflow"
+}
+
+resource "google_project_iam_member" "youtube_fetcher_workflow_run_invoker" {
+  project = var.project_id
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+}
+
+resource "google_workflows_workflow" "youtube_fetcher" {
+  project = var.project_id
+  region = "asia-east1"
+  service_account = google_service_account.youtube_fetcher_workflow.email
+  source_contents = file("${path.module}/youtube-fetcher.yaml")
+}
+
+resource "google_service_account" "youtube_fetcher" {
+  project    = var.project_id
+  account_id = "youtube-fetcher"
+}
