f0a844ba5485c3e24d0ae3b1d4503fb4d3a4143a
Kaito Udagawa <umireon@gmail.com>
2024-02-05 05:02:51 +0900

    Update index.ts

--- serverless/youtube-fetcher/index.ts
+++ serverless/youtube-fetcher/index.ts
@@ -6,25 +6,19 @@ import { type BodyResponseCallback } from "googleapis-common";
 
 const service = google.youtube("v3");
 
-function promisifyYouTubeDataAPI<Params, Response>(
-	func: (params: Params, callback: BodyResponseCallback<Response>) => void
-): (params: Params) => Promise<GaxiosResponse<Response> | null | undefined> {
-	return (params) =>
-		new Promise((resolve, reject) => {
-			func(params, (err, response) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(response);
-				}
-			});
+function listSearch(
+	params: youtube_v3.Params$Resource$Search$List
+): Promise<GaxiosResponse<youtube_v3.Schema$SearchListResponse> | null | undefined> {
+	return new Promise((resolve, reject) => {
+		service.search.list(params, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
 		});
+	});
 }
-
-const listSearch = promisifyYouTubeDataAPI<
-	youtube_v3.Params$Resource$Search$List,
-	youtube_v3.Schema$SearchListResponse
->(service.search.list);
 
 http("youtube-video-fetcher", async (_, res) => {
 	const response = await listSearch({
