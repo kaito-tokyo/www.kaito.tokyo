3bab092986e974f091c0d353e5d929dc29d96c6d
Kaito Udagawa <umireon@gmail.com>
2024-02-05 03:50:56 +0900

    Update index.ts

--- serverless/youtube-fetcher/index.ts
+++ serverless/youtube-fetcher/index.ts
@@ -4,7 +4,9 @@ import { google, youtube_v3 } from "googleapis";
 
 const service = google.youtube("v3");
 
-function listChannels(params: youtube_v3.Params$Resource$Channels$List): Promise<GaxiosResponse<youtube_v3.Schema$ChannelListResponse> | null | undefined> {
+function listChannels(
+	params: youtube_v3.Params$Resource$Channels$List
+): Promise<GaxiosResponse<youtube_v3.Schema$ChannelListResponse> | null | undefined> {
 	return new Promise((resolve, reject) => {
 		service.channels.list(params, (err, response) => {
 			if (err) {
@@ -12,14 +14,14 @@ function listChannels(params: youtube_v3.Params$Resource$Channels$List): Promise
 			} else {
 				resolve(response);
 			}
-		})
+		});
 	});
 }
 
 http("youtube-video-fetcher", async (_, res) => {
 	const response = await listChannels({
 		part: ["snippet"],
-		forUsername: "GoogleDevelopers",
-	})
+		forUsername: "GoogleDevelopers"
+	});
 	res.send(response?.data);
 });
