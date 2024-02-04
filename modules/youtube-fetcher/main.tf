cb5b86a6395912b0d306fdc10420e76bed6ff79d
Kaito Udagawa <umireon@gmail.com>
2024-02-05 07:37:10 +0900

    Fix

--- modules/youtube-fetcher/main.tf
+++ modules/youtube-fetcher/main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "youtube_fetcher_caches" {
+resource "google_storage_bucket" "youtube_fetcher_cache" {
   name                        = "www-kaito-tokyo-youtube-fetcher-cache"
   location                    = "asia-east1"
   project                     = var.project_id
@@ -15,6 +15,12 @@ resource "google_project_iam_member" "youtube_fetcher_workflow_run_invoker" {
   project = var.project_id
   role    = "roles/run.invoker"
   member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+}
+
+resource "google_storage_bucket_iam_member" "youtube_fetcher_workflow_youtube_fetcher_cache_object_admin" {
+  bucket = google_storage_bucket.youtube_fetcher_cache.name
+  role = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
 }
 
 resource "google_workflows_workflow" "youtube_fetcher" {
