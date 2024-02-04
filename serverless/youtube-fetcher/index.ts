60bc32fe779ca89b2b185751c7274208a01daa5d
Kaito Udagawa <umireon@gmail.com>
2024-02-05 04:27:04 +0900

    Update index.ts

--- serverless/youtube-fetcher/index.ts
+++ serverless/youtube-fetcher/index.ts
@@ -1,8 +1,21 @@
 import { http } from "@google-cloud/functions-framework";
 import { type GaxiosResponse } from "gaxios";
 import { google, youtube_v3 } from "googleapis";
+import { GoogleAuth, Impersonated, OAuth2Client } from "google-auth-library";
 
 const service = google.youtube("v3");
+
+async function getAccessTokenFromImpersonatedCredentials() {
+	const googleAuth = new GoogleAuth({
+		scopes: [],
+	});
+	const { credential } = await googleAuth.getApplicationDefault();
+	const impersonatedCredentials = new Impersonated({
+		sourceClient: credential,
+	});
+	const { token } = await impersonatedCredentials.getAccessToken();
+	return token;
+}
 
 function listChannels(
 	params: youtube_v3.Params$Resource$Channels$List
@@ -20,6 +33,9 @@ function listChannels(
 
 http("youtube-video-fetcher", async (_, res) => {
 	const response = await listChannels({
+		auth: new GoogleAuth({
+			scopes: ["https://www.googleapis.com/auth/youtube.readonly"],
+		}),
 		part: ["snippet"],
 		forUsername: "GoogleDevelopers"
 	});
