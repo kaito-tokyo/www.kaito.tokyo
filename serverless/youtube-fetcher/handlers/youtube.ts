4f4bd026dddab5a5e8c6972b941cb1a2763e2810
Kaito Udagawa <umireon@gmail.com>
2024-02-07 01:56:44 +0900

    Fix

---
+++ serverless/youtube-fetcher/handlers/youtube.ts
@@ -0,0 +1,98 @@
+import { type Request, type Response } from "@google-cloud/functions-framework";
+import { type GaxiosResponse } from "gaxios";
+import { google, type youtube_v3 } from "googleapis";
+import { GoogleAuth } from "google-auth-library";
+
+const service = google.youtube("v3");
+const auth = new GoogleAuth({
+	scopes: ["https://www.googleapis.com/auth/youtube.readonly"]
+});
+
+function listSearch(
+	params: youtube_v3.Params$Resource$Search$List
+): Promise<GaxiosResponse<youtube_v3.Schema$SearchListResponse> | null | undefined> {
+	return new Promise((resolve, reject) => {
+		service.search.list(params, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+}
+
+export async function handleListSearch(req: Request, res: Response) {
+	const { channelId, pageToken } = req.query;
+
+	if (typeof channelId !== "string") {
+		throw new Error("channelId is invalid!");
+	}
+
+	if (typeof pageToken !== "undefined" && typeof pageToken !== "string") {
+		throw new Error("pageToken is invalid!");
+	}
+
+	const params: youtube_v3.Params$Resource$Search$List = {
+		auth,
+		part: ["snippet"],
+		channelId,
+		maxResults: 50,
+		order: "date",
+		type: ["video"]
+	};
+
+	if (pageToken) {
+		params.pageToken = pageToken;
+	}
+
+	const response = await listSearch(params);
+
+	res.send(response?.data);
+}
+
+function listVideos(
+	params: youtube_v3.Params$Resource$Videos$List
+): Promise<GaxiosResponse<youtube_v3.Schema$VideoListResponse> | null | undefined> {
+	return new Promise((resolve, reject) => {
+		service.videos.list(params, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+}
+
+export interface YouYubeListVideosRequest {
+	readonly id: string[];
+}
+
+export async function handleListVideos(req: Request, res: Response) {
+	const body: YouYubeListVideosRequest = req.body;
+	const { id } = body;
+
+	if (typeof id === "undefined") {
+		throw new Error("id is invalid!");
+	}
+
+	const response = await listVideos({
+		auth,
+		part: [
+			"contentDetails",
+			"id",
+			"liveStreamingDetails",
+			"localizations",
+			"player",
+			"recordingDetails",
+			"snippet",
+			"statistics",
+			"status",
+			"topicDetails"
+		],
+		id
+	});
+
+	res.send(response?.data);
+}
