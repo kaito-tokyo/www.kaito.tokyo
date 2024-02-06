eeaf7a14c9f1c21ae7293c3755e185fc0cf1cab5
Kaito Udagawa <umireon@gmail.com>
2024-02-07 02:32:45 +0900

    Fix

--- serverless/youtube-fetcher/handlers/digest.ts
+++ serverless/youtube-fetcher/handlers/digest.ts
@@ -2,13 +2,13 @@ import { createHash } from "crypto";
 import { type Request, type Response } from "@google-cloud/functions-framework";
 
 export async function handleDigest(req: Request, res: Response) {
-    const { rawBody } = req;
+	const { rawBody } = req;
 
-    if (typeof rawBody === "undefined") {
-        throw new Error("Body is missing!")
-    }
+	if (typeof rawBody === "undefined") {
+		throw new Error("Body is missing!");
+	}
 
-    const digest = createHash("sha256").update(rawBody).digest("hex");
+	const digest = createHash("sha256").update(rawBody).digest("hex");
 
-    res.send(digest);
+	res.send(digest);
 }
