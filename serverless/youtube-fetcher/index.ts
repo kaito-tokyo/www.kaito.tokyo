c1a6f3693c864362fa98b590b264c10e6518ef0d
Kaito Udagawa <umireon@gmail.com>
2024-02-07 04:52:58 +0900

    Fix

--- serverless/youtube-fetcher/index.ts
+++ serverless/youtube-fetcher/index.ts
@@ -1,6 +1,6 @@
 import { http } from "@google-cloud/functions-framework";
-import { handleDigest } from "./handlers/digest";
-import { handleListSearch, handleListVideos } from "./handlers/youtube";
+import { handleDigest } from "./handlers/digest.js";
+import { handleListSearch, handleListVideos } from "./handlers/youtube.js";
 
 http("youtube-fetcher-digest", handleDigest);
 http("youtube-fetcher-list-search", handleListSearch);
