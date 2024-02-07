72d3d2e6a036fc688654ed05f0c791bb5790fed8
Kaito Udagawa <umireon@gmail.com>
2024-02-08 01:55:18 +0900

    Update youtube.ts

--- serverless/youtube-fetcher/handlers/youtube.ts
+++ serverless/youtube-fetcher/handlers/youtube.ts
@@ -83,6 +83,13 @@ export async function handleSaveListVideos(req: Request, res: Response) {
 		throw new Error("Request body is invalid!");
 	}
 
+	const [outputExists] = await storage.bucket(bucket).file(object).exists()
+
+	if (outputExists) {
+		res.status(204).send("");
+		return;
+	}
+
 	const response = await listVideos({
 		auth,
 		part: [
