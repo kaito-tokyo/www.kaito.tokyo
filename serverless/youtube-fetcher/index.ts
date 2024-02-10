bc3a3346bee7cf442d283145199eec88569f47d4
Kaito Udagawa <umireon@gmail.com>
2024-02-11 01:30:31 +0900

    Update index.ts

--- serverless/youtube-fetcher/index.ts
+++ serverless/youtube-fetcher/index.ts
@@ -2,13 +2,15 @@ import { http } from "@google-cloud/functions-framework";
 import { handleDigest } from "./handlers/digest.js";
 import { handleListSearch } from "./handlers/list-search.js";
 import {
+	handleComposeVideoList
+	handleGenerateListVideosQueries,
 	handleSaveListVideos,
 	handleSplitListVideos,
-	handleGenerateListVideosQueries
 } from "./handlers/list-videos.js";
 
 http("youtube-fetcher-digest", handleDigest);
 http("youtube-fetcher-list-search", handleListSearch);
+http("youtube-fetcher-generate-compose-video-list", handleComposeVideoList);
 http("youtube-fetcher-generate-list-videos-queries", handleGenerateListVideosQueries);
 http("youtube-fetcher-save-list-videos", handleSaveListVideos);
 http("youtube-fetcher-split-list-videos", handleSplitListVideos);
