23e8935016e8f564eb57b20745dae7b7fbf14a71
Kaito Udagawa <umireon@gmail.com>
2024-02-11 04:06:27 +0900

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -11,7 +11,9 @@ export const load = (async () => {
 			select: ["_id", "title", "slug", "images", "publishedAt"]
 		}
 	});
-	const resp1 = await fetch("https://storage.googleapis.com/www-kaito-tokyo-youtube-fetcher-public/UCfhyVWrxCmdUpst-5n7Kz_Q/videos/index.json");
+	const resp1 = await fetch(
+		"https://storage.googleapis.com/www-kaito-tokyo-youtube-fetcher-public/UCfhyVWrxCmdUpst-5n7Kz_Q/videos/index.json"
+	);
 	const json1 = await resp1.json();
 	const resp2 = await fetch(json1.url);
 	const youtubeVideos: youtube_v3.Schema$Video[] = await resp2.json();
