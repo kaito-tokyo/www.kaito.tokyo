f1044842dbf401c35e7c2db8255fd4aa32c2c9fb
Kaito Udagawa <umireon@gmail.com>
2024-02-11 00:00:01 +0900

    Update main.tf (#61)

--- modules/youtube-fetcher/main.tf
+++ modules/youtube-fetcher/main.tf
@@ -1,36 +1,21 @@
-locals {
-  channel_id = "UCfhyVWrxCmdUpst-5n7Kz_Q"
-}
-
+// Service accounts
 resource "google_service_account" "youtube_fetcher_workflow" {
   project    = var.project_id
   account_id = "youtube-fetcher-workflow"
 }
 
-resource "google_project_iam_member" "youtube_fetcher_workflow_run_invoker" {
-  project = var.project_id
-  role    = "roles/run.invoker"
-  member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+resource "google_service_account" "youtube_fetcher" {
+  project    = var.project_id
+  account_id = "youtube-fetcher"
 }
 
-resource "google_project_iam_member" "youtube_fetcher_workflow_log_writer" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
-}
-
+// Cloud Storage Buckets
 resource "google_storage_bucket" "youtube_fetcher_cache" {
   name                        = "www-kaito-tokyo-youtube-fetcher-cache"
   location                    = "asia-east1"
   project                     = var.project_id
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
-}
-
-resource "google_storage_bucket_iam_member" "youtube_fetcher_workflow_youtube_fetcher_cache_object_user" {
-  bucket = google_storage_bucket.youtube_fetcher_cache.name
-  role   = "roles/storage.objectUser"
-  member = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
 }
 
 resource "google_storage_bucket" "youtube_fetcher_metadata" {
@@ -41,15 +26,17 @@ resource "google_storage_bucket" "youtube_fetcher_metadata" {
   public_access_prevention    = "enforced"
 }
 
-resource "google_storage_bucket_iam_member" "youtube_fetcher_workflow_youtube_fetcher_metadata_object_user" {
-  bucket = google_storage_bucket.youtube_fetcher_metadata.name
-  role   = "roles/storage.objectUser"
-  member = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+resource "google_storage_bucket" "youtube_fetcher_public" {
+  name                        = "www-kaito-tokyo-youtube-fetcher-public"
+  location                    = "asia-east1"
+  project                     = var.project_id
+  uniform_bucket_level_access = true
 }
 
+// Cloud Workflows
 locals {
   workflow_env_vars = {
-    CHANNEL_ID           = local.channel_id,
+    CHANNEL_ID           = "UCfhyVWrxCmdUpst-5n7Kz_Q",
     CACHE_BUCKET_NAME    = google_storage_bucket.youtube_fetcher_cache.name,
     METADATA_BUCKET_NAME = google_storage_bucket.youtube_fetcher_metadata.name
   }
@@ -82,11 +69,20 @@ resource "google_workflows_workflow" "fetch_all_videos_list" {
   source_contents = file("${path.module}/workflows/fetch-all-videos-list.yaml")
 }
 
-resource "google_service_account" "youtube_fetcher" {
-  project    = var.project_id
-  account_id = "youtube-fetcher"
+// Project IAM Bindings
+resource "google_project_iam_member" "youtube_fetcher_workflow_run_invoker" {
+  project = var.project_id
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
 }
 
+resource "google_project_iam_member" "youtube_fetcher_workflow_log_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+}
+
+// Cloud Storage IAM Bindings
 resource "google_storage_bucket_iam_member" "youtube_fetcher_cache_object_user" {
   bucket = google_storage_bucket.youtube_fetcher_cache.name
   role   = "roles/storage.objectUser"
@@ -97,4 +93,22 @@ resource "google_storage_bucket_iam_member" "youtube_fetcher_metadata_object_use
   bucket = google_storage_bucket.youtube_fetcher_metadata.name
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.youtube_fetcher.email}"
+}
+
+resource "google_storage_bucket_iam_member" "youtube_fetcher_public_object_user" {
+  bucket = google_storage_bucket.youtube_fetcher_public.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.youtube_fetcher.email}"
+}
+
+resource "google_storage_bucket_iam_member" "youtube_fetcher_workflow_youtube_fetcher_cache_object_user" {
+  bucket = google_storage_bucket.youtube_fetcher_cache.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
+}
+
+resource "google_storage_bucket_iam_member" "youtube_fetcher_workflow_youtube_fetcher_metadata_object_user" {
+  bucket = google_storage_bucket.youtube_fetcher_metadata.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.youtube_fetcher_workflow.email}"
 }
