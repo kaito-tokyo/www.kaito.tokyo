f9e3aaca399292592ca69a6263298893d7f6ce53
Kaito Udagawa <umireon@gmail.com>
2024-02-12 05:21:55 +0900

    Update list-search.ts

--- serverless/youtube-fetcher/handlers/list-search.ts
+++ serverless/youtube-fetcher/handlers/list-search.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "@google-cloud/functions-framework";
 import { type GaxiosResponse } from "gaxios";
 import { type youtube_v3 } from "googleapis";
 
-import { googleAuth, youtubeService } from "./constants.js";
+import { googleAuth, youtubeService, storage } from "./constants.js";
 
 function listSearch(
 	params: youtube_v3.Params$Resource$Search$List
@@ -19,14 +19,15 @@ function listSearch(
 }
 
 export async function handleListSearch(req: Request, res: Response) {
-	const { channelId, pageToken } = req.query;
+	const { channelId, pageToken, outputBucket, outputObject } = req.query;
 
-	if (typeof channelId !== "string") {
-		throw new Error("channelId is invalid!");
-	}
-
-	if (typeof pageToken !== "undefined" && typeof pageToken !== "string") {
-		throw new Error("pageToken is invalid!");
+	if (
+		typeof channelId !== "string" ||
+		(typeof pageToken !== "undefined" && typeof pageToken !== "string") ||
+		typeof outputBucket !== "string" ||
+		typeof outputObject !== "string"
+	) {
+		throw new Error("Query is invalid!");
 	}
 
 	const params: youtube_v3.Params$Resource$Search$List = {
@@ -44,5 +45,7 @@ export async function handleListSearch(req: Request, res: Response) {
 
 	const response = await listSearch(params);
 
-	res.send(response?.data);
+	storage.bucket(outputBucket).file(outputObject).save(JSON.stringify(response?.data));
+
+	res.status(204).send("");
 }
