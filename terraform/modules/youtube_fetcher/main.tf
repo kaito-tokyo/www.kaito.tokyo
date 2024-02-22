93703caf0fd9a15103c77f3f6ecc171860fbcb0d
Kaito Udagawa <umireon@gmail.com>
2024-02-23 01:18:56 +0900

    Fix

--- terraform/modules/youtube_fetcher/main.tf
+++ terraform/modules/youtube_fetcher/main.tf
@@ -84,12 +84,6 @@ resource "google_project_iam_member" "youtube_fetcher_workflow_log_writer" {
 }
 
 // Cloud Storage IAM Bindings
-resource "google_storage_bucket_iam_member" "youtube_fetcher_public_allusers_viewer" {
-  bucket = google_storage_bucket.youtube_fetcher_public.name
-  role   = "roles/storage.objectViewer"
-  member = "allUsers"
-}
-
 resource "google_storage_bucket_iam_member" "youtube_fetcher_cache_object_user" {
   bucket = google_storage_bucket.youtube_fetcher_cache.name
   role   = "roles/storage.objectUser"
