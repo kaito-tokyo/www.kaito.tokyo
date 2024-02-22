c701e8b5af0e19ebac2aec9e13833e3fb6fa8a9d
Kaito Udagawa <umireon@gmail.com>
2024-02-23 02:00:29 +0900

    Update main.tf (#92)

--- terraform/modules/youtube_fetcher/main.tf
+++ terraform/modules/youtube_fetcher/main.tf
@@ -17,6 +17,7 @@ resource "google_service_account" "cloudbuild_youtube_fetcher_functions_main" {
 // Cloud Build Triggers
 resource "google_cloudbuild_trigger" "youtube_fetcher_functions_main" {
   location = "asia-east1"
+  project  = var.project_id
   repository_event_config {
     repository = "kaito-tokyo-www.kaito.tokyo"
     push {
