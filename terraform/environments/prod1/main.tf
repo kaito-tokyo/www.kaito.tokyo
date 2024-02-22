0e4d8ec7770d2e7c6b6d3352a670b7888956917d
Kaito Udagawa <umireon@gmail.com>
2024-02-23 00:50:54 +0900

    Update main.tf

--- terraform/environments/prod1/main.tf
+++ terraform/environments/prod1/main.tf
@@ -2,10 +2,6 @@ locals {
   env = "prod1"
 }
 
-provider "google" {
-  project = var.project_id
-}
-
 module "youtube_fetcher" {
   source     = "../../modules/youtube_fetcher"
   project_id = var.project_id
