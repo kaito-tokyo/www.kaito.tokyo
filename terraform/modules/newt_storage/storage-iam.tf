72c1232a13dfbecf6370c2ec7ab3cb89a40de7db
Kaito Udagawa <umireon@gmail.com>
2024-02-24 02:28:50 +0900

    Update storage-iam.tf (#123)

--- terraform/modules/newt_storage/storage-iam.tf
+++ terraform/modules/newt_storage/storage-iam.tf
@@ -9,3 +9,9 @@ resource "google_storage_bucket_iam_member" "newt_www_kaito_tokyo_www_kaito_toky
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.newt_www_kaito_tokyo.email}"
 }
+
+resource "google_storage_bucket_iam_member" "newt_www_kaito_tokyo_www_kaito_tokyo_object_user" {
+  bucket = google_storage_bucket.www_kaito_tokyo.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.functions.email}"
+}
