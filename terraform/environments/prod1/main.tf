61dfc81aa0272db94cb60ce6b8ce80cbc58605a3
Kaito Udagawa <umireon@gmail.com>
2024-02-23 16:53:05 +0900

    Add newt-storage

--- terraform/environments/prod1/main.tf
+++ terraform/environments/prod1/main.tf
@@ -7,3 +7,8 @@ module "youtube_fetcher" {
   project_id                    = var.project_id
   cloudbuild_trigger_repository = var.cloudbuild_trigger_repository
 }
+
+module "newt_storage" {
+  source     = "../../modules/newt_storage"
+  project_id = var.project_id
+}
