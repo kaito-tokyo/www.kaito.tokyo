ef28de510d0c9f68a2278e72298034d55e54499f
Kaito Udagawa <umireon@gmail.com>
2024-02-23 22:31:09 +0900

    Split files (#108)
    * Split files
    
    * Update iam.tf

---
+++ terraform/modules/newt_storage/storage.tf
@@ -0,0 +1,6 @@
+resource "google_storage_bucket" "newt_storage_www_kaito_tokyo" {
+  name                        = "${var.project_id}-newt-storage-www-kaito-tokyo"
+  location                    = "asia-east1"
+  project                     = var.project_id
+  uniform_bucket_level_access = true
+}
---
+++ terraform/modules/newt_storage/workflows.tf
@@ -0,0 +1,6 @@
+locals {
+  workflow_env_vars = {
+    CHANNEL_ID  = "UCfhyVWrxCmdUpst-5n7Kz_Q",
+    BUCKET_NAME = google_storage_bucket.newt_storage_www_kaito_tokyo.name
+  }
+}
