508d5342a36a157a7d96fb1b5d591cfcc99baf2a
Kaito Udagawa <umireon@gmail.com>
2024-02-23 22:38:55 +0900

    Add optimize-and-upload-image workflow

--- terraform/modules/newt_storage/workflows.tf
+++ terraform/modules/newt_storage/workflows.tf
@@ -1,6 +1,14 @@
 locals {
   workflow_env_vars = {
-    CHANNEL_ID  = "UCfhyVWrxCmdUpst-5n7Kz_Q",
     BUCKET_NAME = google_storage_bucket.newt_storage_www_kaito_tokyo.name
   }
+}
+
+resource "google_workflows_workflow" "optimize_and_upload_image" {
+  name            = "newt-storage-optimize-and-upload-image"
+  project         = var.project_id
+  region          = "asia-east1"
+  service_account = google_service_account.workflow.email
+  user_env_vars   = local.workflow_env_vars
+  source_contents = file("${path.module}/workflows/optimize-and-upload-image.yaml")
 }
