a41f44b9edc3d07b9785115529d0e7857b8de563
Kaito Udagawa <umireon@gmail.com>
2024-02-27 05:54:57 +0900

    Update secret-iam.tf

--- terraform/modules/newt_storage/secret-iam.tf
+++ terraform/modules/newt_storage/secret-iam.tf
@@ -2,12 +2,12 @@ resource "google_secret_manager_secret_iam_member" "functions_cdn_access_key_id"
   project   = var.project_id
   secret_id = google_secret_manager_secret.cdn_access_key_id.secret_id
   role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${google_service_account.newt_www_kaito_tokyo.email}"
+  member    = "serviceAccount:${google_service_account.functions.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "functions_cdn_secret_access_key" {
   project   = var.project_id
   secret_id = google_secret_manager_secret.cdn_secret_access_key.secret_id
   role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${google_service_account.newt_www_kaito_tokyo.email}"
+  member    = "serviceAccount:${google_service_account.functions.email}"
 }
