b80e83762d5e360c66f30da3b1fdf863ac4948d6
Kaito Udagawa <umireon@gmail.com>
2024-02-27 05:45:45 +0900

    Update project-iam.tf

--- terraform/modules/newt_storage/project-iam.tf
+++ terraform/modules/newt_storage/project-iam.tf
@@ -45,3 +45,9 @@ resource "google_project_iam_member" "workflow_run_invoker" {
   role    = "roles/run.invoker"
   member  = "serviceAccount:${google_service_account.workflow.email}"
 }
+
+resource "google_project_iam_member" "workflow_logwriter" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.workflow.email}"
+}
