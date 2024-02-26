1faa4e12a1e2be8030be50eaf21dc0d049af0d4f
Kaito Udagawa <umireon@gmail.com>
2024-02-27 04:45:55 +0900

    LazyGetEnv (#137)
    * LazyGetEnv
    
    * Update env.ts

--- packages/newt-storage/lib/env.ts
+++ packages/newt-storage/lib/env.ts
@@ -6,8 +6,18 @@ export function getEnv(name: string): string {
 	return value;
 }
 
-export const cdnRegion = getEnv("CDN_REGION");
-export const cdnEndpoint = getEnv("CDN_ENDPOINT");
-export const cdnBucketName = getEnv("CDN_BUCKET_NAME");
-export const cdnAccessKeyIdSecretName = getEnv("CDN_ACCESS_KEY_ID_SECRET_NAME");
-export const cdnSecretAccessKeySecretName = getEnv("CDN_SECRET_ACCESS_KEY_SECRET_NAME");
+export function lazyGetEnv(name: string): () => string {
+	let value: string;
+	return () => {
+		if (!value) {
+			value = getEnv(name);
+		}
+		return value;
+	};
+}
+
+export const getCdnRegion = lazyGetEnv("CDN_REGION");
+export const getCdnEndpoint = lazyGetEnv("CDN_ENDPOINT");
+export const getCdnBucketName = lazyGetEnv("CDN_BUCKET_NAME");
+export const getCdnAccessKeyIdSecretName = lazyGetEnv("CDN_ACCESS_KEY_ID_SECRET_NAME");
+export const getCdnSecretAccessKeySecretName = lazyGetEnv("CDN_SECRET_ACCESS_KEY_SECRET_NAME");
