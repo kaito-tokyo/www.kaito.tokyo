75ae9f4315fd8083af766b8434999731b431882c
Kaito Udagawa <umireon@gmail.com>
2024-03-15 04:38:26 +0900

    Fix

--- terraform/modules/youtube_fetcher/project-iam.tf
+++ terraform/modules/youtube_fetcher/project-iam.tf
@@ -10,20 +10,20 @@ resource "google_project_iam_member" "workflow_log_writer" {
   member  = "serviceAccount:${google_service_account.workflow.email}"
 }
 
-resource "google_project_iam_member" "functions_cb_main_log_writer" {
+resource "google_project_iam_member" "workflows_cb_main_logwriter" {
   project = var.project_id
   role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.functions_cb_main.email}"
+  member  = "serviceAccount:${google_service_account.workflows_cb_main.email}"
 }
 
-resource "google_project_iam_member" "functions_cb_main_functions_admin" {
+resource "google_project_iam_member" "workflows_cb_main_functions_admin" {
   project = var.project_id
   role    = "roles/cloudfunctions.admin"
-  member  = "serviceAccount:${google_service_account.functions_cb_main.email}"
+  member  = "serviceAccount:${google_service_account.workflows_cb_main.email}"
 }
 
-resource "google_project_iam_member" "functions_cb_main_run_admin" {
+resource "google_project_iam_member" "workflows_cb_main_run_admin" {
   project = var.project_id
   role    = "roles/run.admin"
-  member  = "serviceAccount:${google_service_account.functions_cb_main.email}"
+  member  = "serviceAccount:${google_service_account.workflows_cb_main.email}"
 }
--- terraform/modules/youtube_fetcher/service-account-iam.tf
+++ terraform/modules/youtube_fetcher/service-account-iam.tf
@@ -1,5 +1,5 @@
-resource "google_service_account_iam_member" "functions_cb_main_service_account_user" {
+resource "google_service_account_iam_member" "workflows_cb_main_serviceaccountuser" {
   service_account_id = google_service_account.functions.name
   role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:${google_service_account.functions_cb_main.email}"
+  member             = "serviceAccount:${google_service_account.workflows_cb_main.email}"
 }
