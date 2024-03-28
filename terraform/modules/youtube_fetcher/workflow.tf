897e6040fc1113a4697b2e685492f5676a0f3603
Kaito Udagawa <umireon@gmail.com>
2024-03-29 04:27:28 +0900

    Fix resource

--- terraform/modules/youtube_fetcher/workflow.tf
+++ terraform/modules/youtube_fetcher/workflow.tf
@@ -1,20 +1,20 @@
-resource "google_cloud_workflow_workflow" "fetch_latest_search_list" {
+resource "google_workflows_workflow" "fetch_latest_search_list" {
   name            = "youtube-fetcher-fetch-latest-search-list"
-  location        = "asia-east1"
+  region          = "asia-east1"
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-latest-search-list.yaml")
 }
 
-resource "google_cloud_workflow_workflow" "fetch_all_search_list" {
+resource "google_workflows_workflow" "fetch_all_search_list" {
   name            = "youtube-fetcher-fetch-all-search-list"
-  location        = "asia-east1"
+  region          = "asia-east1"
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-all-search-list.yaml")
 }
 
-resource "google_cloud_workflow_workflow" "fetch_all_video_list" {
+resource "google_workflows_workflow" "fetch_all_video_list" {
   name            = "youtube-fetcher-fetch-latest-search-list"
-  location        = "asia-east1"
+  region          = "asia-east1"
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-all-video-list.yaml")
 }
