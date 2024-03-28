da75e35e6b0d818727ea6fcd902b96a1124dafb2
Kaito Udagawa <umireon@gmail.com>
2024-03-29 02:53:39 +0900

    Add zip (#168)
    * Fix
    
    * Fmt

---
+++ terraform/modules/youtube_fetcher/function.tf
@@ -0,0 +1,14 @@
+data "archive_file" "function_source" {
+  type       = "zip"
+  source_dir = "."
+  excludes = [
+    "terraform"
+  ]
+  output_path = "/tmp/function-source.zip"
+}
+
+resource "google_storage_bucket_object" "function_source" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.function_source.name
+  source = data.archive_file.function_source.output_path
+}
