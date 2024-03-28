c742ff44033c5b22c66ff8a341ac264520646ecb
Kaito Udagawa <umireon@gmail.com>
2024-03-29 03:53:50 +0900

    Update function.tf

--- terraform/modules/youtube_fetcher/function.tf
+++ terraform/modules/youtube_fetcher/function.tf
@@ -30,3 +30,123 @@ resource "google_cloudfunctions2_function" "digest" {
     service_account_email = google_service_account.function.email
   }
 }
+
+resource "google_cloudfunctions2_function" "save_search_list" {
+  name     = "youtube-fetcher-save-serch-list"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
+
+resource "google_cloudfunctions2_function" "split_search_list" {
+  name     = "youtube-fetcher-split-search-list"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
+
+resource "google_cloudfunctions2_function" "generate_video_list_queries" {
+  name     = "youtube-fetcher-generate-video-list-queries"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
+
+resource "google_cloudfunctions2_function" "save_video_list" {
+  name     = "youtube-fetcher-save-video-list"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
+
+resource "google_cloudfunctions2_function" "split_video_list" {
+  name     = "youtube-fetcher-split-video-list"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
+
+resource "google_cloudfunctions2_function" "compose_video_list" {
+  name     = "youtube-fetcher-compose-video-list"
+  location = "asia-east1"
+
+  build_config {
+    runtime = "nodejs20"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_source.name
+        object = google_storage_bucket_object.function_source.object.name
+      }
+    }
+  }
+
+  service_config {
+    ingress_settings      = "ALLOW_INTERNAL_ONLY"
+    service_account_email = google_service_account.function.email
+  }
+}
