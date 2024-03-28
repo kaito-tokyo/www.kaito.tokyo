4df1e9a28f5088d316111a9c0c89471ffbbe02c9
Kaito Udagawa <umireon@gmail.com>
2024-03-29 04:49:02 +0900

    Remove Cloud Build for Cloud Functions (#182)

--- terraform/modules/youtube_fetcher/service-account.tf
+++ terraform/modules/youtube_fetcher/service-account.tf
@@ -7,8 +7,3 @@ resource "google_service_account" "function" {
   project    = var.project_id
   account_id = "yf-function"
 }
-
-resource "google_service_account" "workflow_cb_main" {
-  project    = var.project_id
-  account_id = "yf-workflow-cb-main"
-}
