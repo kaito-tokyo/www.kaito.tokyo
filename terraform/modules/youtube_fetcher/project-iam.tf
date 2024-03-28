d0003cd8f40fc67510b5649866ad69bb427d9cc9
Kaito Udagawa <umireon@gmail.com>
2024-03-29 04:45:41 +0900

    Remove Cloud Build for Cloud Functions

--- terraform/modules/youtube_fetcher/project-iam.tf
+++ terraform/modules/youtube_fetcher/project-iam.tf
@@ -9,27 +9,3 @@ resource "google_project_iam_member" "workflow_logwriter" {
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.workflow.email}"
 }
-
-resource "google_project_iam_member" "workflow_cb_main_logwriter" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.workflow_cb_main.email}"
-}
-
-resource "google_project_iam_member" "workflow_cb_main_functions_admin" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.admin"
-  member  = "serviceAccount:${google_service_account.workflow_cb_main.email}"
-}
-
-resource "google_project_iam_member" "workflow_cb_main_run_admin" {
-  project = var.project_id
-  role    = "roles/run.admin"
-  member  = "serviceAccount:${google_service_account.workflow_cb_main.email}"
-}
-
-resource "google_project_iam_member" "workflow_cb_main_workflows_admin" {
-  project = var.project_id
-  role    = "roles/workflows.admin"
-  member  = "serviceAccount:${google_service_account.workflow_cb_main.email}"
-}
--- terraform/modules/youtube_fetcher/provider.tf
+++ terraform/modules/youtube_fetcher/provider.tf
@@ -1,4 +1,3 @@
 provider "google" {
   project = var.project_id
-  region  = "asia-east1"
 }
