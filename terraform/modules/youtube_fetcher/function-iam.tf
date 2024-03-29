b980eb59da2fd1a31fa22a35219e778f9f65ba70
Kaito Udagawa <umireon@gmail.com>
2024-03-29 23:42:02 +0900

    Update function-iam.tf

--- terraform/modules/youtube_fetcher/function-iam.tf
+++ terraform/modules/youtube_fetcher/function-iam.tf
@@ -1,34 +1,46 @@
 resource "google_cloudfunctions2_function_iam_member" "workflow_digest_run_invoker" {
+  project        = google_cloudfunctions2_function.digest.project
+  location       = google_cloudfunctions2_function.digest.location
   cloud_function = google_cloudfunctions2_function.digest.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
 }
 
 resource "google_cloudfunctions2_function_iam_member" "workflow_save_search_list_run_invoker" {
+  project        = google_cloudfunctions2_function.save_search_list.project
+  location       = google_cloudfunctions2_function.save_search_list.location
   cloud_function = google_cloudfunctions2_function.save_search_list.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
 }
 
 resource "google_cloudfunctions2_function_iam_member" "workflow_split_search_list_run_invoker" {
+  project        = google_cloudfunctions2_function.split_search_list.project
+  location       = google_cloudfunctions2_function.split_search_list.location
   cloud_function = google_cloudfunctions2_function.split_search_list.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
 }
 
 resource "google_cloudfunctions2_function_iam_member" "workflow_generate_video_list_queries_run_invoker" {
+  project        = google_cloudfunctions2_function.generate_video_list_queries.project
+  location       = google_cloudfunctions2_function.generate_video_list_queries.location
   cloud_function = google_cloudfunctions2_function.generate_video_list_queries.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
 }
 
 resource "google_cloudfunctions2_function_iam_member" "workflow_save_video_list_run_invoker" {
+  project        = google_cloudfunctions2_function.save_video_list.project
+  location       = google_cloudfunctions2_function.save_video_list.location
   cloud_function = google_cloudfunctions2_function.save_video_list.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
 }
 
 resource "google_cloudfunctions2_function_iam_member" "workflow_compose_video_list_run_invoker" {
+  project        = google_cloudfunctions2_function.compose_video_list.project
+  location       = google_cloudfunctions2_function.compose_video_list.location
   cloud_function = google_cloudfunctions2_function.compose_video_list.name
   role           = "roles/run.invoker"
   member         = "serviceAccount:${google_service_account.workflow.email}"
