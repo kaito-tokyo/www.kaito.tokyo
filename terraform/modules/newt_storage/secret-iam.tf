9649f839a344a801e1cd1fef23946699f5855052
Kaito Udagawa <umireon@gmail.com>
2024-03-29 10:03:04 +0900

    Remove project id (#196)
    * Remove project id
    
    * Create main.tf
    
    * Fix
    
    * Update project-iam.tf

--- terraform/modules/newt_storage/secret-iam.tf
+++ terraform/modules/newt_storage/secret-iam.tf
@@ -1,12 +1,10 @@
 resource "google_secret_manager_secret_iam_member" "functions_cdn_access_key_id" {
-  project   = var.project_id
   secret_id = google_secret_manager_secret.cdn_access_key_id.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.function.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "functions_cdn_secret_access_key" {
-  project   = var.project_id
   secret_id = google_secret_manager_secret.cdn_secret_access_key.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.function.email}"
--- terraform/modules/newt_storage/service-account.tf
+++ terraform/modules/newt_storage/service-account.tf
@@ -1,19 +1,15 @@
 resource "google_service_account" "newt_www_kaito_tokyo" {
-  project    = var.project_id
   account_id = "ns-newt-www-kaito-tokyo"
 }
 
 resource "google_service_account" "workflow" {
-  project    = var.project_id
   account_id = "ns-workflow"
 }
 
 resource "google_service_account" "function" {
-  project    = var.project_id
   account_id = "ns-function"
 }
 
 resource "google_service_account" "eventarc" {
-  project    = var.project_id
   account_id = "ns-eventarc"
 }
