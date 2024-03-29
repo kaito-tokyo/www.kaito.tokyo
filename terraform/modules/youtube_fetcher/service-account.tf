cdde5c8431a682182afa50fae1d858dd778de676
Kaito Udagawa <umireon@gmail.com>
2024-03-29 10:24:29 +0900

    Refactor (#198)

--- terraform/modules/youtube_fetcher/service-account.tf
+++ terraform/modules/youtube_fetcher/service-account.tf
@@ -1,9 +1,7 @@
 resource "google_service_account" "workflow" {
-  project    = var.project_id
   account_id = "yf-workflow"
 }
 
 resource "google_service_account" "function" {
-  project    = var.project_id
   account_id = "yf-function"
 }
