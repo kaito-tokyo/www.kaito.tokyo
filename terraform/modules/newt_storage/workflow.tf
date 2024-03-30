91e4ef67a3185ce26f7da92385d7a1f02cdf2360
Kaito Udagawa <umireon@gmail.com>
2024-03-30 22:27:45 +0900

    Fix

--- terraform/modules/newt_storage/workflow.tf
+++ terraform/modules/newt_storage/workflow.tf
@@ -1,6 +1,6 @@
 locals {
   encpoints = {
-    optimizeImage = google_cloud_run_v2_service.optimize_image.uri,
+    optimizeImage     = google_cloud_run_v2_service.optimize_image.uri,
     uploadObjectToCdn = google_cloud_run_v2_service.upload_object_to_cdn.uri,
   }
 }
@@ -9,9 +9,8 @@ resource "google_workflows_workflow" "optimize_and_upload_image" {
   name            = "newt-storage-optimize-and-upload-image"
   region          = "asia-east1"
   service_account = google_service_account.workflow.email
-  user_env_vars   = {
-    ENDPOINTS = jsonencode(local.encpoints),
-    BUCKET_NAME = google_storage_bucket.www_kaito_tokyo.name,
+  user_env_vars = {
+    ENDPOINTS   = jsonencode(local.encpoints),
   }
   source_contents = file("${path.module}/workflows/optimize-and-upload-image.yaml")
 }
