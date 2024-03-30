fd913a1d1b0c1ec36eb9431f01d3e0da951de3a9
Kaito Udagawa <umireon@gmail.com>
2024-03-30 17:28:55 +0900

    Create list-playlistitems.ts

---
+++ packages/youtube-fetcher/handlers/list-playlistitems.ts
@@ -0,0 +1,88 @@
+import { type Request, type Response } from "@google-cloud/functions-framework";
+import { type GaxiosResponse } from "gaxios";
+import { type youtube_v3 } from "googleapis";
+
+import { googleAuth, storage, youtubeService } from "./constants.js";
+
+export function listPlaylistItems(
+	params: youtube_v3.Params$Resource$Playlistitems$List
+): Promise<GaxiosResponse<youtube_v3.Schema$PlaylistItemListResponse> | null | undefined> {
+	return new Promise((resolve, reject) => {
+        youtubeService.playlistItems.list(params, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+}
+
+export async function handleSavePlaylistItemsList(req: Request, res: Response) {
+	const { pageToken, playlistId, outputBucket, outputObject } = req.query;
+
+	if (
+		(typeof pageToken !== "undefined" && typeof pageToken !== "string") ||
+		typeof playlistId !== "string" ||
+		typeof outputBucket !== "string" ||
+		typeof outputObject !== "string"
+	) {
+		throw new Error("Query is invalid!");
+	}
+
+	const [outputExists] = await storage.bucket(outputBucket).file(outputObject).exists();
+
+	if (outputExists) {
+		res.status(204).send("");
+		return;
+	}
+
+	const params: youtube_v3.Params$Resource$Playlistitems$List = {
+		auth: googleAuth,
+		part: ["contentDetails", "id", "snippet", "status"],
+        playlistId,
+		maxResults: 50
+	};
+
+	if (pageToken) {
+		params.pageToken = pageToken;
+	}
+
+	const response = await listPlaylistItems(params);
+
+	await storage.bucket(outputBucket).file(outputObject).save(JSON.stringify(response?.data));
+
+	res.send({
+		nextPageToken: response?.data.nextPageToken
+	});
+}
+
+export async function handleSplitSearchList(req: Request, res: Response) {
+	const { inputBucket, inputObject, outputBucket, outputDirectory } = req.query;
+
+	if (
+		typeof inputBucket !== "string" ||
+		typeof inputObject !== "string" ||
+		typeof outputBucket !== "string" ||
+		typeof outputDirectory !== "string"
+	) {
+		throw new Error("Request query is invalid!");
+	}
+
+	const [response] = await storage.bucket(inputBucket).file(inputObject).download();
+	const json: youtube_v3.Schema$SearchListResponse = JSON.parse(response.toString());
+	if (!json.items) {
+		throw new Error("Input format is invalid!");
+	}
+
+	const outputStorageBucket = storage.bucket(outputBucket);
+	for (const item of json.items) {
+		if (!item.id?.videoId || !item.snippet?.publishedAt) {
+			throw new Error("Item format is invalid");
+		}
+		const outputObject = `${outputDirectory}/${item.snippet.publishedAt} ${item.id.videoId}.json`;
+		await outputStorageBucket.file(outputObject).save(JSON.stringify(item));
+	}
+
+	res.status(204).send("");
+}
