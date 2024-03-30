f896fc309bedce0df39b8b3dba3bb75ccfb608f8
Kaito Udagawa <umireon@gmail.com>
2024-03-30 21:56:42 +0900

    Fix (#218)
    * Fix
    
    * Update run.tf
    
    * Update terraform-plan.yaml
    
    * Update terraform-plan.yaml

---
+++ terraform/modules/newt_storage/run.tf
@@ -0,0 +1,31 @@
+resource "google_cloud_run_v2_service" "optimize_image" {
+  name     = "newt-storage-optimize-image"
+  location = "asia-east1"
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    containers {
+      image   = var.run_image
+      command = ["--target=newt-storage-optimize-image"]
+
+      resources {
+        limits = {
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service" "upload_object_to_cdn" {
+  name     = "newt-storage-upload-object-to-cdn"
+  location = "asia-east1"
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    containers {
+      image   = var.run_image
+      command = ["--target=newt-storage-upload-object-to-cdn"]
+    }
+  }
+}
