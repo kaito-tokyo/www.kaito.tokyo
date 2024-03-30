569c5db7f9c8d5d289e8e67b6f73f7d34cdef0d3
Kaito Udagawa <umireon@gmail.com>
2024-03-30 19:09:44 +0900

    Update output.tf

--- terraform/modules/newt_storage/output.tf
+++
@@ -1,3 +0,0 @@
-output "run_repository" {
-  value = "${google_artifact_registry_repository.run.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.run.repository_id}"
-}
