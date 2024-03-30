e8650828bc8114ba90c6b20fbfeb1d732913e540
Kaito Udagawa <umireon@gmail.com>
2024-03-31 03:13:12 +0900

    add commas

--- terraform/modules/youtube_fetcher/workflow.tf
+++ terraform/modules/youtube_fetcher/workflow.tf
@@ -1,17 +1,17 @@
 locals {
   endpoints = {
-    digest                   = google_cloud_run_v2_service.digest.uri
-    saveSearchList           = google_cloud_run_v2_service.save_search_list.uri
-    splitSearchList          = google_cloud_run_v2_service.split_search_list.uri
-    generateVideoListQueries = google_cloud_run_v2_service.generate_video_list_queries.uri
-    saveVideoList            = google_cloud_run_v2_service.save_video_list.uri
-    splitVideoList           = google_cloud_run_v2_service.split_video_list.uri
-    composeVideoList         = google_cloud_run_v2_service.compose_video_list.uri
+    digest                   = google_cloud_run_v2_service.digest.uri,
+    saveSearchList           = google_cloud_run_v2_service.save_search_list.uri,
+    splitSearchList          = google_cloud_run_v2_service.split_search_list.uri,
+    generateVideoListQueries = google_cloud_run_v2_service.generate_video_list_queries.uri,
+    saveVideoList            = google_cloud_run_v2_service.save_video_list.uri,
+    splitVideoList           = google_cloud_run_v2_service.split_video_list.uri,
+    composeVideoList         = google_cloud_run_v2_service.compose_video_list.uri,
   }
   buckets = {
-    cache    = google_storage_bucket.cache.name
-    metadata = google_storage_bucket.metadata.name
-    public   = google_storage_bucket.public.name
+    cache    = google_storage_bucket.cache.name,
+    metadata = google_storage_bucket.metadata.name,
+    public   = google_storage_bucket.public.name,
   }
   channel_id = "UCfhyVWrxCmdUpst-5n7Kz_Q"
 }
@@ -22,10 +22,10 @@ resource "google_workflows_workflow" "fetch_latest_search_list" {
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-latest-search-list.yaml")
   user_env_vars = {
-    "ENDPOINTS"            = jsonencode(local.endpoints)
-    "CHANNEL_ID"           = local.channel_id
-    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name
-    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name
+    "ENDPOINTS"            = jsonencode(local.endpoints),
+    "CHANNEL_ID"           = local.channel_id,
+    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name,
+    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name,
   }
 }
 
@@ -35,10 +35,10 @@ resource "google_workflows_workflow" "fetch_all_search_list" {
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-all-search-list.yaml")
   user_env_vars = {
-    "ENDPOINTS"            = jsonencode(local.endpoints)
-    "CHANNEL_ID"           = local.channel_id
-    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name
-    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name
+    "ENDPOINTS"            = jsonencode(local.endpoints),
+    "CHANNEL_ID"           = local.channel_id,
+    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name,
+    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name,
   }
 }
 
@@ -48,11 +48,11 @@ resource "google_workflows_workflow" "fetch_all_video_list" {
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-all-video-list.yaml")
   user_env_vars = {
-    "ENDPOINTS"            = jsonencode(local.endpoints)
-    "CHANNEL_ID"           = local.channel_id
-    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name
-    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name
-    "PUBLIC_BUCKET_NAME"   = google_storage_bucket.public.name
+    "ENDPOINTS"            = jsonencode(local.endpoints),
+    "CHANNEL_ID"           = local.channel_id,
+    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name,
+    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name,
+    "PUBLIC_BUCKET_NAME"   = google_storage_bucket.public.name,
   }
 }
 
@@ -62,11 +62,11 @@ resource "google_workflows_workflow" "fetch_all_playlist_items_list" {
   service_account = google_service_account.workflow.email
   source_contents = file("${path.module}/workflows/fetch-all-playlist-items-list.yaml")
   user_env_vars = {
-    "ENDPOINTS"            = jsonencode(local.endpoints)
-    "BUCKETS"              = jsonencode(local.buckets)
-    "CHANNEL_ID"           = local.channel_id
-    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name
-    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name
-    "PUBLIC_BUCKET_NAME"   = google_storage_bucket.public.name
+    "ENDPOINTS"            = jsonencode(local.endpoints),
+    "BUCKETS"              = jsonencode(local.buckets),
+    "CHANNEL_ID"           = local.channel_id,
+    "CACHE_BUCKET_NAME"    = google_storage_bucket.cache.name,
+    "METADATA_BUCKET_NAME" = google_storage_bucket.metadata.name,
+    "PUBLIC_BUCKET_NAME"   = google_storage_bucket.public.name,
   }
 }
