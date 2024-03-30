f03c59c2203f4a731a2ef464ba1ad37af9447b83
Kaito Udagawa <umireon@gmail.com>
2024-03-30 23:07:45 +0900

    Update run.tf (#225)
    * Update run.tf
    
    * Update run.tf

--- terraform/modules/newt_storage/run.tf
+++ terraform/modules/newt_storage/run.tf
@@ -30,6 +30,36 @@ resource "google_cloud_run_v2_service" "upload_object_to_cdn" {
     containers {
       image = var.run_image
       args  = ["--", "--target=newt-storage-upload-object-to-cdn"]
+
+      env {
+        name  = "CDN_REGION"
+        value = var.cdn_region
+      }
+
+      env {
+        name  = "CDN_REGION"
+        value = var.cdn_region
+      }
+
+      env {
+        name  = "CDN_ENDPOINT"
+        value = var.cdn_endpoint
+      }
+
+      env {
+        name  = "CDN_BUCKET_NAME"
+        value = var.cdn_bucket_name
+      }
+
+      env {
+        name  = "CDN_ACCESS_KEY_ID_SECRET_NAME"
+        value = google_secret_manager_secret.cdn_access_key_id.name
+      }
+
+      env {
+        name  = "CDN_SECRET_ACCESS_KEY_SECRET_NAME"
+        value = google_secret_manager_secret.cdn_secret_access_key.name
+      }
     }
   }
 }
