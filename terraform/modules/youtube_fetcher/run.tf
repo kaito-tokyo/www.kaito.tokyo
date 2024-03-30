610c9e8cc1fe519d8a5887eb0b1c88fac521b2bc
Kaito Udagawa <umireon@gmail.com>
2024-03-31 01:27:34 +0900

    Add

--- terraform/modules/youtube_fetcher/run.tf
+++ terraform/modules/youtube_fetcher/run.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_run_v2_service" "youtube_fetcher_digest" {
+resource "google_cloud_run_v2_service" "digest" {
   name     = "youtube-fetcher-digest"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -13,7 +13,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_digest" {
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_save_search_list" {
+resource "google_cloud_run_v2_service" "save_search_list" {
   name     = "youtube-fetcher-save-search-list"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -28,7 +28,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_save_search_list" {
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_split_search_list" {
+resource "google_cloud_run_v2_service" "split_search_list" {
   name     = "youtube-fetcher-split-search-list"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -43,7 +43,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_split_search_list" {
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_generate_video_list_queries" {
+resource "google_cloud_run_v2_service" "generate_video_list_queries" {
   name     = "youtube-fetcher-generate-video-list-queries"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -58,7 +58,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_generate_video_list_quer
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_save_video_list" {
+resource "google_cloud_run_v2_service" "save_video_list" {
   name     = "youtube-fetcher-save-video-list"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -73,7 +73,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_save_video_list" {
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_split_video_list" {
+resource "google_cloud_run_v2_service" "split_video_list" {
   name     = "youtube-fetcher-split-video-list"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -88,7 +88,7 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_split_video_list" {
   }
 }
 
-resource "google_cloud_run_v2_service" "youtube_fetcher_compose_video_list" {
+resource "google_cloud_run_v2_service" "compose_video_list" {
   name     = "youtube-fetcher-compose-video-list"
   location = "asia-east1"
   ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
@@ -102,3 +102,4 @@ resource "google_cloud_run_v2_service" "youtube_fetcher_compose_video_list" {
     }
   }
 }
+
