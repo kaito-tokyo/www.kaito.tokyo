9d506d99172a08eff865f67f9ce0c2cead056dc4
Kaito Udagawa <umireon@gmail.com>
2024-03-31 01:29:51 +0900

    Youtube fetcher/add run (#239)
    * Fix
    
    * Add
    
    * Update workflow.tf

--- terraform/environments/prod1/main.tf
+++ terraform/environments/prod1/main.tf
@@ -6,14 +6,15 @@ module "youtube_fetcher" {
   source                        = "../../modules/youtube_fetcher"
   project_id                    = var.project_id
   cloudbuild_trigger_repository = var.cloudbuild_trigger_repository
+  run_image                     = var.run_image
 }
 
 module "newt_storage" {
   source                        = "../../modules/newt_storage"
   project_id                    = var.project_id
   cloudbuild_trigger_repository = var.cloudbuild_trigger_repository
+  run_image                     = var.run_image
   cdn_region                    = var.cdn_region
   cdn_endpoint                  = var.cdn_endpoint
   cdn_bucket_name               = var.cdn_bucket_name
-  run_image                     = var.run_image
 }
