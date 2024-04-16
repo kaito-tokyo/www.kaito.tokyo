24508f8ba89359c285332a7c574ffdc9b09b975c
Kaito Udagawa <umireon@gmail.com>
2024-04-17 05:08:12 +0900

    Separate buckets (#281)
    * Fix
    
    * Fic
    
    * Update storage-iam.tf

--- terraform/modules/newt_storage/storage-iam.tf
+++ terraform/modules/newt_storage/storage-iam.tf
@@ -1,17 +1,23 @@
-resource "google_storage_bucket_iam_member" "allusers_www_kaito_tokyo_object_viewer" {
-  bucket = google_storage_bucket.www_kaito_tokyo.name
+resource "google_storage_bucket_iam_member" "allusers_www_kaito_tokyo_objectviewer" {
+  bucket = google_storage_bucket.input_www_kaito_tokyo.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
 
-resource "google_storage_bucket_iam_member" "newt_www_kaito_tokyo_www_kaito_tokyo_object_user" {
-  bucket = google_storage_bucket.www_kaito_tokyo.name
+resource "google_storage_bucket_iam_member" "newt_www_kaito_tokyo_input_www_kaito_tokyo_objectuser" {
+  bucket = google_storage_bucket.input_www_kaito_tokyo.name
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.newt_www_kaito_tokyo.email}"
 }
 
-resource "google_storage_bucket_iam_member" "functions_www_kaito_tokyo_object_user" {
-  bucket = google_storage_bucket.www_kaito_tokyo.name
+resource "google_storage_bucket_iam_member" "run_input_www_kaito_tokyo_objectviewer" {
+  bucket = google_storage_bucket.input_www_kaito_tokyo.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.run.email}"
+}
+
+resource "google_storage_bucket_iam_member" "run_optimized_www_kaito_tokyo_objectuser" {
+  bucket = google_storage_bucket.optimized_www_kaito_tokyo.name
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.run.email}"
 }
--- terraform/modules/newt_storage/workflow.tf
+++ terraform/modules/newt_storage/workflow.tf
@@ -1,7 +1,10 @@
 locals {
-  encpoints = {
+  endpoints = {
     optimizeImage     = google_cloud_run_v2_service.optimize_image.uri,
     uploadObjectToCdn = google_cloud_run_v2_service.upload_object_to_cdn.uri,
+  }
+  buckets = {
+    optimized = google_storage_bucket.optimized_www_kaito_tokyo.name,
   }
 }
 
@@ -10,7 +13,8 @@ resource "google_workflows_workflow" "optimize_and_upload_image" {
   region          = "asia-east1"
   service_account = google_service_account.workflow.email
   user_env_vars = {
-    ENDPOINTS = jsonencode(local.encpoints),
+    BUCKETS   = jsonencode(local.buckets),
+    ENDPOINTS = jsonencode(local.endpoints),
   }
   source_contents = file("${path.module}/workflows/optimize-and-upload-image.yaml")
 }
