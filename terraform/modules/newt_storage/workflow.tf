4cec33ca8a87c8d807a848629a049ffe90629a72
Kaito Udagawa <umireon@gmail.com>
2024-04-17 06:09:39 +0900

    Add all images (#283)
    * Add all images
    
    * Update optimize-and-upload-all-images.yaml

--- terraform/modules/newt_storage/workflow.tf
+++ terraform/modules/newt_storage/workflow.tf
@@ -4,6 +4,7 @@ locals {
     uploadObjectToCdn = google_cloud_run_v2_service.upload_object_to_cdn.uri,
   }
   buckets = {
+    input     = google_storage_bucket.input_www_kaito_tokyo.name,
     optimized = google_storage_bucket.optimized_www_kaito_tokyo.name,
   }
 }
@@ -17,4 +18,15 @@ resource "google_workflows_workflow" "optimize_and_upload_image" {
     ENDPOINTS = jsonencode(local.endpoints),
   }
   source_contents = file("${path.module}/workflows/optimize-and-upload-image.yaml")
+}
+
+resource "google_workflows_workflow" "optimize_and_upload_all_images" {
+  name            = "newt-storage-optimize-and-upload-all-images"
+  region          = "asia-east1"
+  service_account = google_service_account.workflow.email
+  user_env_vars = {
+    BUCKETS   = jsonencode(local.buckets),
+    ENDPOINTS = jsonencode(local.endpoints),
+  }
+  source_contents = file("${path.module}/workflows/optimize-and-upload-all-images.yaml")
 }
