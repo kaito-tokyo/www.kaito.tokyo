9105fa1a885849f7d7a234f0256879722052ef3e
Kaito Udagawa <umireon@gmail.com>
2024-04-17 08:44:15 +0900

    Update optimize-image.ts

--- packages/newt-storage/handlers/optimize-image.ts
+++ packages/newt-storage/handlers/optimize-image.ts
@@ -24,37 +24,37 @@ export async function handleOptimizeImage(req: Request, res: Response) {
 
 	const outputObjects: string[] = [];
 
-    const addOptimizedImage = async (name: string, image: sharp.Sharp) => {
-        const buffer = await image.toBuffer();
-        await storage.bucket(outputBucket).file(name).save(buffer);
-        outputObjects.push(name);
+	const addOptimizedImage = async (name: string, image: sharp.Sharp) => {
+		const buffer = await image.toBuffer();
+		await storage.bucket(outputBucket).file(name).save(buffer);
+		outputObjects.push(name);
 
-        const metadataName = `${name}.metadata.json`;
-        const metadata = await sharp(buffer).metadata();
-        await storage.bucket(outputBucket).file(metadataName).save(JSON.stringify(metadata));
-        
-        outputObjects.concat(name, metadataName)
-    }
+		const metadataName = `${name}.metadata.json`;
+		const metadata = await sharp(buffer).metadata();
+		await storage.bucket(outputBucket).file(metadataName).save(JSON.stringify(metadata));
 
-    await addOptimizedImage(
-        `${inputObjectDirname}/_thumbnail400/${inputObjectBasename}.webp`,
-        sharp(response).resize(400, 400, { fit: "contain" }).webp()
-    )
+		outputObjects.concat(name, metadataName);
+	};
 
-    await addOptimizedImage(
-        `${inputObjectDirname}/_thumbnail400/${inputObjectBasename}.png`,
-        sharp(response).resize(400, 400, { fit: "contain" }).png()
-    )
+	await addOptimizedImage(
+		`${inputObjectDirname}/_thumbnail400/${inputObjectBasename}.webp`,
+		sharp(response).resize(400, 400, { fit: "contain" }).webp()
+	);
 
-    await addOptimizedImage(
-        `${inputObjectDirname}/_artwork1600/${inputObjectBasename}.webp`,
-        sharp(response).resize(1600, 1600, { fit: "contain" }).webp()
-    )
+	await addOptimizedImage(
+		`${inputObjectDirname}/_thumbnail400/${inputObjectBasename}.png`,
+		sharp(response).resize(400, 400, { fit: "contain" }).png()
+	);
 
-    await addOptimizedImage(
-        `${inputObjectDirname}/_artwork1600/${inputObjectBasename}.png`,
-        sharp(response).resize(1600, 1600, { fit: "contain" }).png()
-    )
+	await addOptimizedImage(
+		`${inputObjectDirname}/_artwork1600/${inputObjectBasename}.webp`,
+		sharp(response).resize(1600, 1600, { fit: "contain" }).webp()
+	);
+
+	await addOptimizedImage(
+		`${inputObjectDirname}/_artwork1600/${inputObjectBasename}.png`,
+		sharp(response).resize(1600, 1600, { fit: "contain" }).png()
+	);
 
 	res.send({ outputObjects });
 }
