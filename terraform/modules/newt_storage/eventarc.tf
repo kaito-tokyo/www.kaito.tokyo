bfaf639fe28e75b3747c19aa6bff7228c9a16e26
Kaito Udagawa <umireon@gmail.com>
2024-04-17 05:05:35 +0900

    Fic

--- terraform/modules/newt_storage/eventarc.tf
+++ terraform/modules/newt_storage/eventarc.tf
@@ -1,4 +1,4 @@
-resource "google_eventarc_trigger" "optimize_and_upload_image_www_kaito_tokyo" {
+resource "google_eventarc_trigger" "optimize_and_upload_input_image_www_kaito_tokyo" {
   name     = "optimize-and-upload-image-www-kaito-tokyo"
   location = "asia-east1"
 
@@ -9,7 +9,7 @@ resource "google_eventarc_trigger" "optimize_and_upload_image_www_kaito_tokyo" {
 
   matching_criteria {
     attribute = "bucket"
-    value     = google_storage_bucket.www_kaito_tokyo.name
+    value     = google_storage_bucket.input_www_kaito_tokyo.name
   }
 
   destination {
