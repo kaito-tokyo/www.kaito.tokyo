486d67373dfb723a23dc42f081d9c72a5700c381
Kaito Udagawa <umireon@gmail.com>
2024-04-17 08:27:33 +0900

    Fix

--- packages/www/src/routes/artworks/[slug]/+page.svelte
+++ packages/www/src/routes/artworks/[slug]/+page.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
-	import { error } from "@sveltejs/kit";
 	import { formatInTimeZone } from "date-fns-tz";
+
+	import { error } from "@sveltejs/kit";
+
+	import type { PageData } from "./$types";
+
+	import { formatPublishedAt } from "$lib/newt/images";
+
 	export let data: PageData;
+
 	if (!data.article) {
 		error(404);
 	}
-	const { article } = data;
 
-	function formatDate(dateString: string): string {
-		return formatInTimeZone(new Date(dateString), "Asia/Tokyo", "yyyy-MM-dd");
-	}
+	const { article } = data;
 </script>
 
 <svelte:head>
 	<title>{article.title}</title>
-	<meta name="description" content="投稿詳細ページです" />
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300"
-	/>
+	<meta name="description" content={`${article.title}: ${article.description}`} />
 </svelte:head>
 
 <section>
 	<article>
 		<h1>{article.title}</h1>
-		<div class="metadata">
-			<p>投稿日：{formatDate(article.publishedAt)}</p>
-		</div>
+		<section class="metadata">
+			<p>投稿日：{formatPublishedAt(article.publishedAt)}</p>
+		</section>
 		<div>{@html article.description}</div>
 		{#each article.images as image}
-			<img src={image.src} layout="constrained" width={1600} height={1600} />
+			<img src={image.src} layout="constrained" width={800} height={800} />
 		{/each}
 	</article>
 </section>
@@ -42,11 +41,13 @@
 		padding: 10px;
 	}
 
-	article h1 {
-		font-family: "Zen Kaku Gothic New", serif;
-	}
-
 	article .metadata {
 		text-align: right;
+	}
+
+	article img {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
 	}
 </style>
