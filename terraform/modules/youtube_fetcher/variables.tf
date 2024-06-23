e1c445382abd63d204d232929638b8823f7dfd96
Kaito Udagawa <umireon@gmail.com>
2024-06-23 19:17:39 +0900

    Update variables.tf

--- terraform/modules/youtube_fetcher/variables.tf
+++ terraform/modules/youtube_fetcher/variables.tf
@@ -3,24 +3,19 @@ variable "project_id" {
   type        = string
 }
 
-variable "cloudbuild_trigger_repository" {
-  description = "cloudbuild trigger repository"
+variable "region" {
+  description = "region of the cloud services"
   type        = string
 }
 
-variable "channel_ids" {
+variable "youtube_fetcher_channel_ids" {
   description = "list of channel ids to fetch"
   type        = list(string)
 }
 
-variable "playlist_ids" {
+variable "youtube_fetcherplaylist_ids" {
   description = "list of playlist ids to fetch"
   type        = list(string)
-}
-
-variable "region" {
-  description = "region of the cloud services"
-  type        = string
 }
 
 variable "run_image" {
