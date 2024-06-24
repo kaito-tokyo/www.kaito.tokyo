0895d2d3693040b38f98964ade9466ec37e3cde2
Kaito Udagawa <umireon@gmail.com>
2024-06-24 10:19:45 +0900

    Update index.ts

--- packages/newt-storage/index.ts
+++ packages/newt-storage/index.ts
@@ -1,2 +1,14 @@
-export { handleOptimizeImage } from "./handlers/optimize-image.js";
-export { handleUploadObjectToCdn } from "./handlers/upload-object-to-cdn.js";
+import express from "express";
+
+import { handleOptimizeImage } from "./handlers/optimize-image.js";
+import { handleUploadObjectToCdn } from "./handlers/upload-object-to-cdn.js";
+
+const app = express();
+
+app.get("/newt-storage-optimize-image", handleOptimizeImage);
+app.get("/newt-storage-upload-object-to-cdn", handleUploadObjectToCdn);
+
+const port = parseInt(process.env["PORT"] ?? "8080", 10);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
