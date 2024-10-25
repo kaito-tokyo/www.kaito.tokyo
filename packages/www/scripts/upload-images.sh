2fcb3cb9a4eac7b136a35cb1053146b6a420eeb2
Kaito Udagawa <umireon@gmail.com>
2024-10-25 17:22:59 +0900

    Fix

--- packages/www/scripts/upload-images.sh
+++ packages/www/scripts/upload-images.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-wrangler
+BUCKET=www-img-kaito-tokyo
+
+for artwork_dir in src/routes/artworks/*
+do
+  prefix=${artwork_dir#src/routes/}
+  for image in $artwork_dir/*.{png,webp}
+  do
+    name=${image##*/}
+    npx wrangler r2 object put "$BUCKET/$prefix/$name" -f "$image"
+  done
+done
