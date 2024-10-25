1ac5740fb69d3c1388119c98b5199972c7065332
Kaito Udagawa <umireon@gmail.com>
2024-10-25 17:34:27 +0900

    Update upload-images.sh

--- packages/www/scripts/upload-images.sh
+++ packages/www/scripts/upload-images.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -euo pipefail
+
 BUCKET=www-img-kaito-tokyo
 
-for artwork_dir in src/routes/artworks/*
+for artwork_dir in src/routes/artworks/2*
 do
   prefix=${artwork_dir#src/routes/}
   for image in $artwork_dir/*.{png,webp}
   do
     name=${image##*/}
-    npx wrangler r2 object put "$BUCKET/$prefix/$name" -f "$image"
+    echo npx wrangler r2 object put "$BUCKET/$prefix/$name" -f "$image"
   done
 done
