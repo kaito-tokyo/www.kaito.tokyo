4be2596f10eac9598ba1fcbae48d15b6c0a48e41
Kaito Udagawa <umireon@gmail.com>
2024-10-26 07:18:10 +0900

    Update upload-images.sh

--- packages/www/scripts/upload-images.sh
+++ packages/www/scripts/upload-images.sh
@@ -6,8 +6,8 @@ BUCKET=www-img-kaito-tokyo
 
 if ! command -v rclone
 then
-  apt-get update
-  apt-get install -y --no-install-recommends rclone
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends rclone
 fi
 
 rclone --version
