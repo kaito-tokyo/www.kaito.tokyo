2278fd94d806707a51cf91b4ee486e027187780a
Kaito Udagawa <umireon@gmail.com>
2024-11-22 07:35:54 +0900

    Fix

--- packages/www/src/routes/(artworks)/artworks/2024-10-04-angel-vapereon/+page.svelte
+++ packages/www/src/routes/(artworks)/artworks/2024-10-04-angel-vapereon/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import { dev } from "$app/environment"
 	const slug = import.meta.url.split("/").slice(-2)[0];
-	let wwwImg: string;
-	if (dev) {
-		wwwImg = `.`;
-	} else {
-		wwwImg = `https://www-img.kaito.tokyo/artworks/${slug}`;
-	}
-
-	import image01 from "./2024-10-04-angel-vapereon_01.png";
+	const wwwImg = `https://www-img.kaito.tokyo/artworks/${slug}`;
 </script>
 
 <svelte:head>
@@ -24,7 +17,7 @@
 		ポケモン天使化の企画に参加しました
 	</section>
 	<img
-		src={image01}
+		src={`${wwwImg}/2024-10-04-angel-vapereon_01.png`}
 		width="1600"
 		height="1600"
 		alt="天使シャワーズ"
