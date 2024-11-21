ac136747a2e79de1f20fe385f896c90ed6dcbcb8
Kaito Udagawa <umireon@gmail.com>
2024-11-22 08:27:11 +0900

    Add 777 followers eevee (#448)
    * Add
    
    * Create thumbnail400.webp

---
+++ packages/www/src/routes/(artworks)/artworks/2024-11-22-777-followers-eevee/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	const wwwImg = `https://www-img.kaito.tokyo/artworks/2024-11-22-777-followers-eevee`;
+</script>
+
+<svelte:head>
+	<title>777フォロワー記念イーブイ</title>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="og:type" content="article" />
+	<meta property="og:image" content={`${wwwImg}/2024-11-22-777-followers-eevee_01.jpg`} />
+	<meta property="og:title" content="777フォロワー記念イーブイ" />
+</svelte:head>
+
+<article>
+	<h1>777フォロワー記念イーブイ</h1>
+	<section class="metadata">
+		<p>投稿日:<time datetime="2024-11-22">2024-11-22</time></p>
+	</section>
+	<section class="description">一日遅れですね</section>
+	<img
+		src={`${wwwImg}/2024-11-22-777-followers-eevee_01.jpg`}
+		width="1600"
+		height="1600"
+		alt="777フォロワー記念イーブイ"
+	/>
+</article>
